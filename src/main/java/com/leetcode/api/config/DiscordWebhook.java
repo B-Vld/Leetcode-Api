93c28ba4899cc66d86cefc1b8f7a6dc9d65d6c88
Vlad Beiusanu <vlad.beiusanu@gmail.com>
2022-05-16 21:51:06 +0300

    another cron change

--- src/main/java/com/leetcode/api/config/DiscordWebhook.java
+++ src/main/java/com/leetcode/api/config/DiscordWebhook.java
@@ -26,7 +26,7 @@ public class DiscordWebhook {
     @Autowired
     private DailyChallengeService dailyChallengeService;
 
-    @Scheduled(cron = "0 30 20 * * *", zone = "GMT+3")
+    @Scheduled(cron = "0 15 19 * * *", zone = "GMT+3")
     public void sendWebhookRequest() throws IOException {
         var maybeDailyChallenge = dailyChallengeService.fetchDailyChallenge();
         if (maybeDailyChallenge.isPresent()) {
