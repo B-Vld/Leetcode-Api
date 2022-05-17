6a8e67aefa0dff2780dd09353ed28be96d65741a
Vlad Beiusanu <vlad.beiusanu@gmail.com>
2022-05-17 10:17:24 +0300

    added embed support for discord webhook

---
+++ src/main/java/com/leetcode/api/model/discord/Field.java
@@ -0,0 +1,14 @@
+package com.leetcode.api.model.discord;
+
+import lombok.Data;
+
+@Data
+public class Field {
+
+    private final String name;
+
+    private final String value;
+
+    private final Boolean inline;
+
+}
---
+++ src/main/java/com/leetcode/api/model/discord/Thumbnail.java
@@ -0,0 +1,10 @@
+package com.leetcode.api.model.discord;
+
+import lombok.Data;
+
+@Data
+public class Thumbnail {
+
+    private final String url;
+
+}
